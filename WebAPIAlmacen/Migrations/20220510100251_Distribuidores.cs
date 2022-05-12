8de0a63cdba625bf8944a2a8a6b5dfcf32b6ced2
JAVIER PASCUAL <fpascual@iconmm.com>
2022-05-12 12:42:40 +0200

    Proteccion inicial con los ficheros locales

---
+++ WebAPIAlmacen/Migrations/20220510100251_Distribuidores.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebAPIAlmacen.Migrations
+{
+    public partial class Distribuidores : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Distribuidores",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Distribuidores", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DistribuidorProducto",
+                columns: table => new
+                {
+                    DistribuidoresId = table.Column<int>(type: "int", nullable: false),
+                    ProductosId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DistribuidorProducto", x => new { x.DistribuidoresId, x.ProductosId });
+                    table.ForeignKey(
+                        name: "FK_DistribuidorProducto_Distribuidores_DistribuidoresId",
+                        column: x => x.DistribuidoresId,
+                        principalTable: "Distribuidores",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_DistribuidorProducto_Productos_ProductosId",
+                        column: x => x.ProductosId,
+                        principalTable: "Productos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DistribuidorProducto_ProductosId",
+                table: "DistribuidorProducto",
+                column: "ProductosId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DistribuidorProducto");
+
+            migrationBuilder.DropTable(
+                name: "Distribuidores");
+        }
+    }
+}
---
+++ WebAPIAlmacen/Migrations/20220510101337_DistribuidoresProductos.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebAPIAlmacen.Migrations
+{
+    public partial class DistribuidoresProductos : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DistribuidoresProductos",
+                columns: table => new
+                {
+                    DistribuidorId = table.Column<int>(type: "int", nullable: false),
+                    ProductoId = table.Column<int>(type: "int", nullable: false),
+                    UnidadesVendidas = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DistribuidoresProductos", x => new { x.DistribuidorId, x.ProductoId });
+                    table.ForeignKey(
+                        name: "FK_DistribuidoresProductos_Distribuidores_DistribuidorId",
+                        column: x => x.DistribuidorId,
+                        principalTable: "Distribuidores",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_DistribuidoresProductos_Productos_ProductoId",
+                        column: x => x.ProductoId,
+                        principalTable: "Productos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DistribuidoresProductos_ProductoId",
+                table: "DistribuidoresProductos",
+                column: "ProductoId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DistribuidoresProductos");
+        }
+    }
+}
